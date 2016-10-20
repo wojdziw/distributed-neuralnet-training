5d0c577b6f355b69180b357233f073eb54a18adf
Wojciech <wojciech.dziwulski@gmail.com>
2016-10-20 18:22:40 +0800

    more preparing to add my own architectures

--- Code/simple-deep/code/training.sh
+++ Code/simple-deep/code/training.sh
@@ -1,1 +1,1 @@
-~/usr/caffe/build/tools/caffe train --solver ../models/solver.prototxt 2>&1 | tee ../models/model_1_train.log
+CUDA_VISIBLE_DEVICES=3 $CAFFE_ROOT/caffe/build/tools/caffe train --solver ../models/solver.prototxt 2>&1 | tee ../models/model-train.log
