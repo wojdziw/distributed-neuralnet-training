5995597b4e6c89df815b6b3eea3fe85d7fb9580a
Wojciech <wojciech.dziwulski@gmail.com>
2016-10-21 12:02:58 +0800

    Added the new net architecture

--- Code/two-nets/code/print-architecture.sh
+++ Code/two-nets/code/print-architecture.sh
@@ -1,1 +1,1 @@
-python $CAFFE_ROOT/python/draw_net.py ../models/net_train.prototxt ../models/caffe_model_1.png
+python $CAFFE_ROOT/python/draw_net.py ../models/net-train.prototxt ../models/model.png
