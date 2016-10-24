9a607714c3f94d02de315a634ea3a2249c98c85c
Wojciech Dziwulski <wojciech.dziwulski@gmail.com>
2016-10-24 13:07:50 +0800

    playing with the lmdb generation for net2, although an image can't have 96 layers...

--- Code/two-nets/code/simple_net2_input.py
+++ Code/two-nets/code/simple_net2_input.py
@@ -1,17 +1,19 @@
 import numpy as np
 import cv2
 
-generate_white_input(no_filters, image_size, file_names, output_path):
-	image = np.zeros([no_filters, image_size[0], image_size[1]])
+def generate_white_input(no_filters, image_size, file_names, output_path):
+	image = np.zeros([image_size[0], image_size[1], 3])
 
 	for file_name in file_names:
+		file_name = output_path+file_name.split("/")[3]
+		print file_name
 		cv2.imwrite(file_name, image)
 
 	return 0
 
-no_filters = 96
+no_filters = 3
 image_size = [13,13]
 file_names = np.load('../comms/image_data.npy')
-output_path = "../input/net2_train"
+output_path = "../input/net2_train/"
 
 generate_white_input(no_filters, image_size, file_names, output_path)
