b5dd74d9441652d87d9a45690915fc20eb4b8006
Wojciech <wojciech.dziwulski@gmail.com>
2016-10-24 12:27:57 +0800

    generating dummy input for net2

--- Code/two-nets/code/simple_net2_input.py
+++ Code/two-nets/code/simple_net2_input.py
@@ -1,11 +1,17 @@
 import numpy as np
+import cv2
 
-generate_white_input(dataset_size, no_filters, image_size, labels, output_path):
-	images = 
+generate_white_input(no_filters, image_size, file_names, output_path):
+	image = np.zeros([no_filters, image_size[0], image_size[1]])
+
+	for file_name in file_names:
+		cv2.imwrite(file_name, image)
 
 	return 0
 
-dataset_size = 400
 no_filters = 96
 image_size = [13,13]
-labels = np.load('image_data')
+file_names = np.load('../comms/image_data.npy')
+output_path = "../input/net2_train"
+
+generate_white_input(no_filters, image_size, file_names, output_path)
