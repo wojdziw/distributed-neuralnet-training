a2dfe747084f50767c9bd5179ef824b4f632a4ae
Wojciech Dziwulski <wojciech.dziwulski@gmail.com>
2016-11-25 18:04:22 +0800

    Proper version of the repo

--- Code/two-nets/code/net1_training.py
+++ Code/two-nets/code/net1_training.py
@@ -10,6 +10,7 @@ stepPerIter = 1
 learningRate = 0.00000001
 
 losses = np.zeros(maxIter)
+ithoughtlosses = np.zeros(maxIter)
 
 net2_iteration = -1
 
@@ -69,16 +70,22 @@ for net1_iteration in range(maxIter):
 	for layer in solver.net.layers:
     		for blob in layer.blobs:
         		blob.data[...] -= learningRate*blob.diff
+	
 	print "Sum of conv3p is: " + str(np.sum(solver.net.blobs['conv3p'].data))
+	
 	difference = solver.net.blobs['conv3p'].data-solver.net.blobs['conv3'].data
 	loss = np.sum(difference**2)/solver.net.blobs['conv3p'].num/2
-	# get the value of the loss
+	
+	# save the value of the loss
 	losses[net1_iteration] = float(solver.net.blobs['loss'].data)
+	ithoughtlosses[net1_iteration] = loss 
+
 	print "Loss is: " + str(float(solver.net.blobs['loss'].data))
 	print "And I thought it would be:" + str(loss)
 	print "Iteration " + str(net1_iteration) + ": net1 back pass finish"
 	
 	np.save('../models/snapshots/net1_losses', losses)
+	np.save('../models/snapshots/net1_perceived_losses', ithoughtlosses)
 	if net1_iteration%100==0: #and net1_iteration>0:
 		solver.net.save('../models/snapshots/net1_iter'+str(net1_iteration)+'.caffemodel')	
 
