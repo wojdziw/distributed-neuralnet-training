afc5b836f0953b9140b8914a81eea9422df0e079
wojdziw <wojciech.dziwulski@gmail.com>
2017-01-23 22:23:44 +0800

    Changed some plotting

--- Code/cifar10/code/fetch_and_plot_losses.sh
+++ Code/cifar10/code/fetch_and_plot_losses.sh
@@ -1,4 +1,5 @@
-scp fyp:~/Final-Year-Project/Code/cifar10/models/snapshots/net1_losses.npy ../models/snapshots
-scp fyp:~/Final-Year-Project/Code/cifar10/models/snapshots/net2_losses.npy ../models/snapshots
-scp fyp:~/Final-Year-Project/Code/cifar10/models/snapshots/net12_losses.npy ../models/snapshots
+scp fyp:~/Final-Year-Project/Code/cifar10/models/snapshots/first-try/net1_losses.npy ../models/snapshots/first-try
+scp fyp:~/Final-Year-Project/Code/cifar10/models/snapshots/first-try/net1_perceived_losses.npy ../models/snapshots/first-try
+scp fyp:~/Final-Year-Project/Code/cifar10/models/snapshots/first-try/net2_losses.npy ../models/snapshots/first-try
+scp fyp:~/Final-Year-Project/Code/cifar10/models/snapshots/first-try/net12_losses.npy ../models/snapshots/first-try
 python plotting_losses.py
